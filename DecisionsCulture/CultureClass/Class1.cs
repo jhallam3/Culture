dbf276e5f37e76c1248559a80e748827ae69b5e0
jon <jon.hallam@managed-desktop.com>
2019-07-16 14:52:00 +0100

    Initial.

---
+++ DecisionsCulture/CultureClass/Class1.cs
@@ -0,0 +1,21 @@
+﻿using DecisionsFramework.Design.Flow;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDateFormat
+{
+    [AutoRegisterMethodsOnClass(true, "Integration", "DateFormat", RegisterForAgents = true)]
+    public class GetDateFormat
+    {
+        public string CurrentCultureName()
+        {
+            return CultureInfo.CurrentCulture.Name;
+        }
+
+
+    }
+}
